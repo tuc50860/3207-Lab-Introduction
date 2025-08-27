0c53d6d82dee89104b6ffb115660ec164d13bdd6
Albert Alvarado <tuc50860@temple.edu>
2025-08-27 19:22:54 -0400

    a message

--- print_random.c
+++ print_random.c
@@ -2,7 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+/*
+* Summary: Function that generates a random char between A-Z
+* Input: None
+* Output: char between A-Z (Uppercase as stated in lab instructions.
+*/
+char randchar()
+{
+	return (char)('A' + (rand()%26))
+}//close randchar()
 
 int main()
 {
